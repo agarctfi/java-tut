d76e5a95e4104e600d148a5367eff4fd2486bd18
Alfredo Garcia <alfredo.garcia@hallmark.edu>
2024-08-06 15:49:51 -0500

    Data Types

--- MyFirstProgram/MyFirstProgram/src/App.java
+++ MyFirstProgram/MyFirstProgram/src/App.java
@@ -42,6 +42,56 @@ public class App {
 
         // Backslash
         System.out.println("This is a backslash: \\");
+
+        // byte
+        byte b = 100;
+        byte minByte = -128;
+        byte maxByte = 127;
+
+        // short
+        short s = 10000;
+        short minShort = -32768;
+        short maxShort = 32767;
+
+        // int
+        int i = 100000;
+        int minInt = -2147483648;
+        int maxInt = 2147483647;
+
+        // long
+        long l = 100000L;
+        long minLong = -9223372036854775808L;
+        long maxLong = 9223372036854775807L;
+
+        // float
+        float f = 10.5f;
+        float minFloat = 1.4E-45f;
+        float maxFloat = 3.4028235E38f;
+
+        // double
+        double d = 10.5;
+        double minDouble = 4.9E-324;
+        double maxDouble = 1.7976931348623157E308;
+
+        // char
+        char c = 'A';
+        char minChar = '\u0000';
+        char maxChar = '\uffff';
+
+        // boolean
+        boolean bool = true;
+        boolean boolFalse = false;
+
+        // Print all values
+        System.out.println("\n\n\nData Types Examples:");
+        System.out.println("byte: " + b + ", min: " + minByte + ", max: " + maxByte);
+        System.out.println("short: " + s + ", min: " + minShort + ", max: " + maxShort);
+        System.out.println("int: " + i + ", min: " + minInt + ", max: " + maxInt);
+        System.out.println("long: " + l + ", min: " + minLong + ", max: " + maxLong);
+        System.out.println("float: " + f + ", min: " + minFloat + ", max: " + maxFloat);
+        System.out.println("double: " + d + ", min: " + minDouble + ", max: " + maxDouble);
+        System.out.println("char: " + c + ", min: " + (int)minChar + ", max: " + (int)maxChar);
+        System.out.println("boolean: " + bool + ", " + boolFalse);
        
     }
 }
