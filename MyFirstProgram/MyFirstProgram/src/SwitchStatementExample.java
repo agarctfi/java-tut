1d4b00667ff046255bbe1529983ab0baac3f8ef4
Alfredo Garcia <alfredo.garcia@hallmark.edu>
2024-08-07 00:22:03 -0500

    SwitchStatementExample

---
+++ MyFirstProgram/MyFirstProgram/src/SwitchStatementExample.java
@@ -0,0 +1,16 @@
+public class SwitchStatementExample {
+    public static void main(String[] args) {
+        int dayOfWeek = 3;
+
+        switch (dayOfWeek) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("Invalid day of the week.");
+        }
+    }
+}
